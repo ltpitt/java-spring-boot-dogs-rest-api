0d83bfe1e67a4f9ef7eb062ca45fcf62330b46ca
Davide Nastri <d.nastri@gmail.com>
2020-07-31 23:29:19 +0200

    feat: Fixed all Warnings

---
+++ src/main/java/it/davidenastri/dogs/service/DogService.java
@@ -0,0 +1,12 @@
+package it.davidenastri.dogs.service;
+
+import it.davidenastri.dogs.entity.Dog;
+
+import java.util.List;
+
+public interface DogService {
+    List<Dog> retrieveDogs();
+    List<String> retrieveDogBreed();
+    String retrieveDogBreedById(Long id);
+    List<String> retrieveDogNames();
+}
---
+++ src/main/java/it/davidenastri/dogs/service/DogServiceImpl.java
@@ -0,0 +1,32 @@
+package it.davidenastri.dogs.service;
+
+import it.davidenastri.dogs.entity.Dog;
+import it.davidenastri.dogs.repository.DogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DogServiceImpl implements DogService {
+    @Autowired
+    DogRepository dogRepository;
+
+    public List<Dog> retrieveDogs() {
+        return (List<Dog>) dogRepository.findAll();
+    }
+
+    public List<String> retrieveDogBreed() {
+        return dogRepository.findAllBreed();
+    }
+
+    public String retrieveDogBreedById(Long id) {
+        Optional<String> optionalBreed = Optional.ofNullable(dogRepository.findBreedById(id));
+        return optionalBreed.orElseThrow(DogNotFoundException::new);
+    }
+
+    public List<String> retrieveDogNames() {
+        return dogRepository.findAllName();
+    }
+}
